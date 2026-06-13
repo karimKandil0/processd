cc699ec58eac306a79eb9da3600bc547f45cd0b4
karimKandil0 <kimo989yt@gmail.com>
2026-06-13 15:18:14 +0300

    feat: wrote error enum

---
+++ processd-core/src/error.rs
@@ -0,0 +1,18 @@
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum ConfigError {
+    #[error("IO error: {0}")]
+    Io(#[from] std::io::Error),
+
+    #[error("TOML parse error: {0}")]
+    Toml(#[from] toml::de::Error),
+
+    #[error("unknown dependency '{dep}' required by '{service}'")]
+    UnknownDependency { service: String, dep: String },
+
+    #[error("dependency cycle detected involving '{0}'")]
+    CycleDetected(String),
+}
+
+
